1b32ef009eacf7094b9afad62cf395f0b54fe5c5
jef balmes <jefbalmes@gmail.com>
2026-08-15 13:01:15 +0800

    added details to show which items are installed to a pc

--- src/components/MemoryDetail.vue
+++ src/components/MemoryDetail.vue
@@ -20,21 +20,37 @@ const props = defineProps({
       <h4 v-if="!item.available" class="text-[11px] text-slate-500 font-inter">{{ formattedAmount(item.profit) }}</h4>
     </div>
   
-    <div @click.prevent="soldStore.toggleSold(item)" v-if="item.available" class="absolute top-2 right-7">
-      <svg class="w-4 h-4 text-slate-600" width="64px" height="64px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="currentColor" d="M704 288h131.072a32 32 0 0 1 31.808 28.8L886.4 512h-64.384l-16-160H704v96a32 32 0 1 1-64 0v-96H384v96a32 32 0 0 1-64 0v-96H217.92l-51.2 512H512v64H131.328a32 32 0 0 1-31.808-35.2l57.6-576a32 32 0 0 1 31.808-28.8H320v-22.336C320 154.688 405.504 64 512 64s192 90.688 192 201.664v22.4zm-64 0v-22.336C640 189.248 582.272 128 512 128c-70.272 0-128 61.248-128 137.664v22.4h256zm201.408 476.16a32 32 0 1 1 45.248 45.184l-128 128a32 32 0 0 1-45.248 0l-128-128a32 32 0 1 1 45.248-45.248L704 837.504V608a32 32 0 1 1 64 0v229.504l73.408-73.408z"></path></g></svg>
+    <div 
+      v-if="item.micro" 
+      class="absolute top-2 right-2 text-xs font-inter flex justify-center items-center gap-1"
+      :class="{
+        'text-sky-600' : item.micro?.brand === 'dell',
+        'text-rose-500' : item.micro?.brand === 'lenovo',
+        'text-gray-400' : item.micro?.brand === 'hp',
+      }"
+    >
+      <svg class="w-4 h-4" width="64px" height="64px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M8,2h8a2,2,0,0,1,2,2V20a2,2,0,0,1-2,2H8a2,2,0,0,1-2-2V4A2,2,0,0,1,8,2M8,4V6h8V4H8m8,4H8v2h8V8m0,10H14v2h2Z"></path> <rect width="24" height="24" fill="none"></rect> </g>
+      </svg>
+      {{ `${item.micro?.series} (${item.micro?.id})` }}
     </div>
-  
-    <div v-if="item.available" class="absolute top-2 right-2 flex flex-col items-end text-slate-600">
-      <CopyToClipboard v-slot="{ status, copy }">
-        <button
-          @click="copy(item.text)"
-          :disabled="status === 'copied'"
-          class="cursor-pointer"
-          >
-          <svg class="w-4 h-4" fill="currentColor" width="64px" height="64px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" transform="rotate(270)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M21,8H9A1,1,0,0,0,8,9V21a1,1,0,0,0,1,1H21a1,1,0,0,0,1-1V9A1,1,0,0,0,21,8ZM20,20H10V10H20ZM6,15a1,1,0,0,1-1,1H3a1,1,0,0,1-1-1V3A1,1,0,0,1,3,2H15a1,1,0,0,1,1,1V5a1,1,0,0,1-2,0V4H4V14H5A1,1,0,0,1,6,15Z"></path></g></svg>
-        </button>
-        <small v-if="status === 'copied'" class="mt-1 text-xs font-inter text-green-600">Copied!</small>
-      </CopyToClipboard>
+    <div v-else>
+      <div @click.prevent="soldStore.toggleSold(item)" v-if="item.available" class="absolute top-2 right-7">
+        <svg class="w-4 h-4 text-slate-600" width="64px" height="64px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="currentColor" d="M704 288h131.072a32 32 0 0 1 31.808 28.8L886.4 512h-64.384l-16-160H704v96a32 32 0 1 1-64 0v-96H384v96a32 32 0 0 1-64 0v-96H217.92l-51.2 512H512v64H131.328a32 32 0 0 1-31.808-35.2l57.6-576a32 32 0 0 1 31.808-28.8H320v-22.336C320 154.688 405.504 64 512 64s192 90.688 192 201.664v22.4zm-64 0v-22.336C640 189.248 582.272 128 512 128c-70.272 0-128 61.248-128 137.664v22.4h256zm201.408 476.16a32 32 0 1 1 45.248 45.184l-128 128a32 32 0 0 1-45.248 0l-128-128a32 32 0 1 1 45.248-45.248L704 837.504V608a32 32 0 1 1 64 0v229.504l73.408-73.408z"></path></g></svg>
+      </div>
+    
+      <div v-if="item.available" class="absolute top-2 right-2 flex flex-col items-end text-slate-600">
+        <CopyToClipboard v-slot="{ status, copy }">
+          <button
+            @click="copy(item.text)"
+            :disabled="status === 'copied'"
+            class="cursor-pointer"
+            >
+            <svg class="w-4 h-4" fill="currentColor" width="64px" height="64px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" transform="rotate(270)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M21,8H9A1,1,0,0,0,8,9V21a1,1,0,0,0,1,1H21a1,1,0,0,0,1-1V9A1,1,0,0,0,21,8ZM20,20H10V10H20ZM6,15a1,1,0,0,1-1,1H3a1,1,0,0,1-1-1V3A1,1,0,0,1,3,2H15a1,1,0,0,1,1,1V5a1,1,0,0,1-2,0V4H4V14H5A1,1,0,0,1,6,15Z"></path></g></svg>
+          </button>
+          <small v-if="status === 'copied'" class="mt-1 text-xs font-inter text-green-600">Copied!</small>
+        </CopyToClipboard>
+      </div>
     </div>
   
     <div class="flex text-slate-500 pb-2">
